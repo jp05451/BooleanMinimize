90d5188c7b0bab7fd01570365e2d941b431a12c6
jp05451 <jp05451@yahoo.com.tw>
2021-11-18 03:30:27 +0800

    2021/11/18

--- main.cpp
+++ main.cpp
@@ -5,22 +5,7 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-    // Py_Initialize();
-
-    // PyObject *pModule = NULL;
-    // PyObject *pFunc = NULL;
-    // PyObject *pInstance = NULL;
-
-    // pModule = PyImport_ImportModule("BooleanMinimize");
-    // pFunc = PyObject_GetAttrString(pModule, "BooleanMinimize");
-    // pInstance = PyEval_CallObject(pFunc, NULL);
-    // PyObject_CallMethod(pInstance,"input.pla","test.pla");
-
-    // Py_Finalize();
-
-    // return 0;
-
-    system("pip3 install pipenv");
+    //system("pip3 install pipenv");
     system("pipenv install");
     // system("pipenv shell");
     string command = "pipenv run python3 BooleanMinimize.py";
@@ -30,6 +15,8 @@ int main(int argc, char **argv)
     command += argv[2];
     cout << command << endl;
     system(command.c_str());
+
+    system("pipenv --rm");
 
     return 0;
 }
