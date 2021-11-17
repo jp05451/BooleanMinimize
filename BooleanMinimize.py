3f67b1d250288f852c83c2ceb4a5625d45a4630f
jp05451 <jp05451@yahoo.com.tw>
2021-11-17 16:38:24 +0800

    second commit

--- BooleanMinimize.py
+++ BooleanMinimize.py
@@ -1,26 +1,33 @@
-# from pyeda.inter import *
+from pyeda.inter import *
+from copy import deepcopy
 import logicmin
 import sys
 
 
 def dontCareVar(inputList, outputList):
+    # no "-" in the string
     if(inputList[0].find("-") == -1):
-        outputList.append(inputList)
-        # print(inputList)
+        if(inputList not in outputList):
+            outputList.append(inputList)
         return
 
     pos = inputList[0].find("-")
 
+    # switch the "-" to 0 or 1
     t = list(inputList[0])
     t1 = list(inputList[0])
     t[pos] = "0"
     t1[pos] = "1"
 
+    # put it in back to the inputList
     inputList[0] = "".join(t)
-    dontCareVar(inputList, outputList)
-
+    # recursive to swich string
+    dontCareVar(deepcopy(inputList), outputList)
+    
+    # put it in back to the inputList
     inputList[0] = "".join(t1)
-    dontCareVar(inputList, outputList)
+    # recursive to swich string
+    dontCareVar(deepcopy(inputList), outputList)
 
 
 # inputFile = open(sys.argv[0], "r", encoding="utf8")
@@ -48,11 +55,10 @@ for i in range(commandNumber):
     temp = inputFile.readline().split(" ")
     temp[1] = temp[1].replace("\n", "")
     if(temp[0].find("-") != -1):
-        dontCareVar(temp, commandList)
+        dontCareVar(deepcopy(temp), commandList)
+    else:
+        commandList.append(temp)
 
-
-# print(commandList)
-print()
 
 # insert the truth table to the mininizer
 t = logicmin.TT(inputVar, outputVar)
@@ -60,7 +66,15 @@ for i in range(commandNumber):
     t.add(commandList[i][0], commandList[i][1])
 sol = t.solve()
 
-print(sol.printN(xnames=["a", "b", "c", "d"], ynames=["y"], info=True))
+for i in commandList:
+    print(i)
+
+print(sol.printN(xnames=[i for i in inputVarList],
+      ynames=[j for j in outputVarList], info=False))
+
+# X = exprvars('x', 4)
+# f1 = truthtable(X, "0000011111------")
+# print(X)
 
 
 inputFile.close()
